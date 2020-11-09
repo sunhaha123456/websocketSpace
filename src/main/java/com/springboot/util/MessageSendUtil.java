4db488072198f07a020ea0b80694ebdbe49f10e5
sunp <sunp@dcnoahark.com>
2020-11-09 10:12:24 +0800

    websocket 优化

--- src/main/java/com/springboot/util/MessageSendUtil.java
+++ src/main/java/com/springboot/util/MessageSendUtil.java
@@ -1,22 +1,18 @@
 package com.springboot.util;
 
-import com.springboot.websocket.MessageSendWebsocket;
-
 import javax.websocket.Session;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class MessageSendUtil {
 
     //J.U.C包下线程安全的类，主要用来存放每个客户端对应的webSocket连接
-    public static CopyOnWriteArraySet<MessageSendWebsocket> websocketSet = new CopyOnWriteArraySet<MessageSendWebsocket>();
+    public static CopyOnWriteArraySet<Session> websocketSet = new CopyOnWriteArraySet<Session>();
 
     // 群发消息
     public static void sendMessageAll(String message) {
-        for (MessageSendWebsocket ws : websocketSet) {
+        for (Session session : websocketSet) {
             try {
-                Session session = ws.getSession();
-                closeSession(session);
-                session.getBasicRemote().sendText(message + " sessionId：" + session.getId());
+                session.getBasicRemote().sendText("sessionId：" + session.getId() + "，发送消息：" + message);
             } catch (Exception e) {
                 System.out.println("群发消息错误，错误信息：" + e);
             }
@@ -27,6 +23,7 @@ public class MessageSendUtil {
         try {
             if (session != null && !session.isOpen()) {
                 session.close();
+                websocketSet.remove(session);
             }
         } catch (Exception e) {
             System.out.println("websocket 关闭 session报错，错误信息：" + e);
--- src/main/java/com/springboot/websocket/MessageSendWebsocket.java
+++ src/main/java/com/springboot/websocket/MessageSendWebsocket.java
@@ -10,45 +10,32 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/messageSend")
 public class MessageSendWebsocket {
 
-    // 每个客户端都会有相应的session,服务端可以发送相关消息
-    private Session session;
-
     // 功能：打开连接。进入页面后会自动发请求到此进行连接
     @OnOpen
     public void onOpen(Session session) {
-        this.session = session;
-        MessageSendUtil.websocketSet.add(this);
-        System.out.println("websocket有新的连接，总数：" + MessageSendUtil.websocketSet.size());
+        MessageSendUtil.websocketSet.add(session);
+        System.out.println("websocket有新的链接，sessionId：" + session.getId() + "，总链接数变为：" + MessageSendUtil.websocketSet.size());
     }
 
     // 功能：用户关闭页面（即关闭连接）时触发
     @OnClose
-    public void onClose() {
+    public void onClose(Session session) {
         try {
-            MessageSendUtil.closeSession(this.getSession());
-            MessageSendUtil.websocketSet.remove(this);
-            System.out.println("websocket有连接断开，总数：" + MessageSendUtil.websocketSet.size());
+            MessageSendUtil.closeSession(session);
+            System.out.println("websocket有链接断开，sessionId：" + session.getId() + "，总链接数变为：" + MessageSendUtil.websocketSet.size());
         } catch (Exception e) {
-            System.out.println("关闭链接时发生错误，错误信息：" + e);
+            System.out.println("websocket关闭链接时发生错误，错误信息：" + e);
         }
     }
 
     // 功能：接收客户端发送来的消息
     @OnMessage
-    public void onMessage(String message) {
-        System.out.println("websocket收到客户端发来的消息：" + message);
+    public void onMessage(Session session, String message) {
+        System.out.println("sessionId：" + session.getId() + "，接受到消息：" + message);
     }
 
     @OnError
     public void onError(Session session, Throwable error) {
-        System.out.println("发生错误，错误信息：" + error);
-    }
-
-    public Session getSession() {
-        return session;
-    }
-
-    public void setSession(Session session) {
-        this.session = session;
+        System.out.println("sessionId：" + session.getId() + "，发生错误，错误信息：" + error);
     }
 }
